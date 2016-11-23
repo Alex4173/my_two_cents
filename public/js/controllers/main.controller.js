4cd0adde58e17f43d3c198420b7efdc252ed4759
Alex Jones <alex.l.jones.3@gmail.com>
2016-11-22 20:49:38 -0500

    implemented get one by id route

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -9,7 +9,16 @@
     $scope.create = create;
     $scope.deletePost = deletePost;
     $scope.updatePost = updatePost;
+    $scope.getOne = getOne;
+    $scope.getSelectedPost = getSelectedPost;
 
+    function getOne(id){
+      PostService.getOne(id);
+    }
+
+    function getSelectedPost(){
+      $scope.selectedPost = PostService.getSelectedPost();
+    }
 
     $scope.$watch(function(){
       return PostService.getAll();
@@ -32,5 +41,5 @@
       $scope.updatedPost = {};
     }
   }
-  
+
 }());
